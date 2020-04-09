062f545e403a9c4cddd5954d1a8537f3103fcb51
Olivier Noumbi <olivierfaustin@yahoo.fr>
2020-04-09 00:16:49 -0400

    Implemented main logic and injected some styling

--- js/todos.js
+++ js/todos.js
@@ -1,4 +1,23 @@
-$("li").click(function () {
+//Check Off Specific Todos by clicking
 
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
+
+
+//Click on X to delete the todo
+
+$("ul").on("click","span",function(){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+})
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X </span>"+ todoText + "</li>");
+    }
+   
+})
